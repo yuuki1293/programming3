6ae65fa3ec65723de6b99a5266446689575be388
yuuki1293 <yuuki1293@outlook.jp>
2022-09-30 10:22:07 +0900

    リスト3.17

--- Sample03_04_Input.java
+++ Sample03_04_Input.java
@@ -3,9 +3,9 @@ import java.util.*
 public class Sample03_04_Input {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("数値を入力してください：");
-		int a;
-		a = sc.nextInt();
-		System.out.println("入力された数値："+a);
+		System.out.println("文字列を入力してください：");
+		String a;
+		a = sc.nextLine();
+		System.out.println("入力された文字列：" + a);
 	}
 }
