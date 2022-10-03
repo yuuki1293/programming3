63ca6355272d51aff3e45ef5d090cdc824a34f9a
yuuki1293 <yuuki1293@outlook.jp>
2022-10-03 10:31:09 +0900

    5.8問2

---
+++ src/Prob05_02.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Prob05_02 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+        	for (int j = 0; j < i; j++) {
+        		System.out.print("■");
+        	}
+        	System.out.println();
+        }
+    }
+}
