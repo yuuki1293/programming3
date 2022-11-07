51a6c70ebb1af759ee9f4cc4b503a9fd17be7183
yuuki1293 <yuuki1293@outlook.jp>
2022-11-07 10:04:24 +0900

    fix: error

--- src/ArrayGraph.java
+++ src/ArrayGraph.java
@@ -2,7 +2,7 @@ public class ArrayGraph {
 	public static void main(String[] args) {
 		int[] array = new int[5];
 		for(int i = 0; i < array.length; i++) {
-			array[i] = i
+			array[i] = i;
 		}
 	}
 }
