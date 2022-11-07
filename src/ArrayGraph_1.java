996311f297e3333f7ddf53c7b949ac35c706f0b6
yuuki1293 <yuuki1293@outlook.jp>
2022-11-07 10:14:38 +0900

    6.8問3 改良

--- src/ArrayGraph_1.java
+++ src/ArrayGraph_1.java
@@ -1,7 +1,9 @@
-public class ArrayGraph {
+public class ArrayGraph_1 {
 	public static void main(String[] args) {
+		String[] subjects = {"数","英","理","社","国"};
 		int[] data = {6,8,7,5,9};
 		for(int i = 0; i< data.length; i++) {
+			System.out.print(subjects[i]);
 			for(int x = 0; x < data[i];x++) {
 				System.out.print("*");
 			}
