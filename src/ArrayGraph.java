d602e07f06c9dab6f34e15708bbbfc08d79ddf15
yuuki1293 <yuuki1293@outlook.jp>
2022-11-07 10:06:06 +0900

    6.8問2

--- src/ArrayGraph.java
+++ src/ArrayGraph.java
@@ -2,7 +2,10 @@ public class ArrayGraph {
 	public static void main(String[] args) {
 		int[] array = new int[5];
 		for(int i = 0; i < array.length; i++) {
-			array[i] = i;
+			array[i] = i * 2 + 1;
+		}
+		for(int i = array.length-1; i >= 0; i--) {
+			System.out.println(array[i]);
 		}
 	}
 }
