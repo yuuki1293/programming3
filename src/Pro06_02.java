21e6fb36c42addb5258acfd3e59478027e153d3b
yuuki1293 <yuuki1293@outlook.jp>
2022-11-07 10:01:04 +0900

    6.8問1

--- src/Pro06_02.java
+++ src/Pro06_02.java
@@ -1,9 +1,13 @@
 public class Pro06_02 {
 	public static void main(String[] args) {
 		int[] tests = {62,84,78,50,91};
+		int m = tests[0];
 		for(int i =0;i<tests.length;i++) {
 			System.out.print(tests[i]+" ");
+			if(m<tests[i])
+				m=tests[i];
 		}
 		System.out.println();
+		System.out.println("最大値：" + m);
 	}
 }
