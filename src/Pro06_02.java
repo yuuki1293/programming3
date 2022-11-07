ba5f4416bd1e3338c37d34c70148456282cad59f
yuuki1293 <yuuki1293@outlook.jp>
2022-11-07 09:58:55 +0900

    リスト6.10

---
+++ src/Pro06_02.java
@@ -0,0 +1,9 @@
+public class Pro06_02 {
+	public static void main(String[] args) {
+		int[] tests = {62,84,78,50,91};
+		for(int i =0;i<tests.length;i++) {
+			System.out.print(tests[i]+" ");
+		}
+		System.out.println();
+	}
+}
